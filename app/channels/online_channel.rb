500ae70107a54abf16401737e5985d3848d420e5
Rawgher <rdpender3@gmail.com>
2020-06-30 16:55:19 -0700

    working on online user channel

---
+++ app/channels/online_channel.rb
@@ -0,0 +1,13 @@
+class OnlineChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "online_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def appear
+    ActionCable.server.broadcast "online_channel", message: "#{current_user.username} has joined"
+  end
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
      user = User.find_by(username: params[:session][:username])
      if user && user.authenticate(params[:session][:password])
        session[:user_id] = user.id
+       cookies[:user_id] = current_user.id
        flash[:success] = "You have successfully logged in"
        redirect_to root_path
      else
@@ -18,6 +19,7 @@ class SessionsController < ApplicationController
 
    def destroy
      session[:user_id] = nil
+     cookies[:user_id] = nil
      flash[:success] = "You have successfully logged out"
      redirect_to login_path
    end
